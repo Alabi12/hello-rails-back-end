92cf65bf1fe8012a8abd328ea0cb8454d5afac2b
Alabi12 <wolo22001@gmail.com>
2023-04-06 10:29:55 +0000

    Add the front end domain to connect the rails app and configured the cors

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -1,2 +1,4 @@
+# This module contains helper methods for the RootController.
 module RootHelper
+  # Helper methods for the RootController go here
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
+# This is the base mailer class that other mailers in the application inherit from.
+# It inherits from ActionMailer::Base, the base class for mailers in Ruby on Rails.
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
